a58825b028a39f2ad7824a6802f7d0ae97e7a042
RRTECH1 <uraut0818@gmail.com>
2024-10-16 13:55:13 +0530

    update

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+//lavel 1
 const ball = document.getElementById('Ball');
 let isJumping = false;
 let interval;
+let stopCount = 0; 
+const stopCountDisplay = document.getElementById('stopCountDisplay'); 
 
 ball.addEventListener('click', () => {
   if (!isJumping) {
     startJumping();
   } else {
-    stopJumping()
+    stopJumping();
   }
 });
 
@@ -15,18 +18,26 @@ function startJumping() {
   let position = 0;
   let direction = 1;
   const maxPosition = 500;
+  
+  // lavel 2
 
   interval = setInterval(() => {
     if (position >= maxPosition) direction = -1;
     if (position <= 0) direction = 1;
 
-    position += 10 * direction;
-
+    position += 10 * direction; 
     ball.style.transform = `translateY(${position}px)`;
-  }, 20);
+  }, 10);Great! It sounds like you're ready to create a website for RR TECH, a software company. Could you provide a bit more detail about the types of software products RR TECH offers? This will help me tailor the website for your business.
 }
 
 function stopJumping() {
   clearInterval(interval); 
-  isJumping = false
+  isJumping = false;
+  stopCount++; 
+  stopCountDisplay.textContent = `score ${stopCount}`; 
 }
+//lavel 3
+ 
+ let function gameover() {
+   
+ }
