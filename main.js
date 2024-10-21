a1fff15aa257d4a31535ba52c958f95b84c8a12e
RRTECH1 <uraut4236@gmail.com>
2024-10-22 02:30:42 +0530

    modified index.html

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-//lavel 1
+// lavel 1  
 const ball = document.getElementById('Ball');
 let isJumping = false;
 let interval;
-let stopCount = 0; 
-const stopCountDisplay = document.getElementById('stopCountDisplay'); 
+let gameTimeout;
+let timeRemaining = 30; 
+const stopCountDisplay = document.getElementById('stopCountDisplay');
+const gameOverDisplay = document.getElementById('gameOverDisplay');  
+const timerDisplay = document.getElementById('timerDisplay'); 
 
 ball.addEventListener('click', () => {
   if (!isJumping) {
@@ -13,28 +16,56 @@ ball.addEventListener('click', () => {
   }
 });
 
+//lavel 2
+
 function startJumping() {
   isJumping = true;
   let position = 0;
   let direction = 1;
   const maxPosition = 500;
+
   
-  // lavel 2
+  if (gameTimeout) {
+    clearTimeout(gameTimeout);
+  }
+
+
+  timeRemaining = 30;
+  timerDisplay.textContent = `Time: ${timeRemaining}`;
+
+  
+  gameTimeout = setTimeout(() => {
+    gameOver();
+  }, 300000); 
+
+  const timeInterval = setInterval(() => {
+    timeRemaining--;
+    timerDisplay.textContent = `Time: ${timeRemaining}s`;
+
+    if (timeRemaining <= 0) {
+      clearInterval(timeInterval); 
+    }
+  }, 1000); 
 
   interval = setInterval(() => {
     if (position >= maxPosition) direction = -1;
     if (position <= 0) direction = 1;
 
-    position += 10 * direction; 
+    position += 5 * direction;
     ball.style.transform = `translateY(${position}px)`;
   }, 10);
 }
 
+//lavel 3 gaee over 
 function stopJumping() {
-  clearInterval(interval); 
+  clearInterval(interval);
   isJumping = false;
-  stopCount++; 
-  stopCountDisplay.textContent = `score ${stopCount}`; 
+  stopCount++;
+  stopCountDisplay.textContent = `score: ${stopCount}`;
 }
-//lavel 3
- 
+
+//lavel 4 
+
+function gameOver(){
+  
+}
